07432ed6ca5b68e80e7966ba6cf48818cc1432e8
LaraFernandesz <lara.marina@aluno.uepb.edu.br>
2025-08-26 15:57:33 -0300

    adicionando pesquisar po titulo

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,8 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Biblioteca {
-    public static void main(String[] args) {
-        Usuario user1 = new Usuario(123, "Thiago");
-        Usuario user2 = new Usuario(123, "Thiago");
-        System.out.println(user1.hashCode());
-        System.out.println(user2.hashCode());
+
+    //private Livro[] acervo;
+    //private int livrosNoAcervo;
+    private List<Livro> acervo;
+    private List<Usuario> listaUsuarios;
+
+    public Biblioteca(){
+        //acervo = new Livro[5];
+        //livrosNoAcervo = 0;
+        acervo = new ArrayList<>();
+        listaUsuarios = new ArrayList<>();
+
     }
+    public void cadastrarLivro(Livro livro){
+        acervo.add(livro);
+        System.out.println("Livro " + livro.getTitulo() + " adicionando no acervo.");
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroEncontrado = null;
+        for(Livro l : this.acervo){
+            if(l.getTitulo().equalsIgnoreCase(titulo)){
+                return l;
+            }
+        }
+        return null;
+    }
+
+    //public void cadastrarLivro(Livro livro){
+    //if(livrosNoAcervo < acervo.length){
+    //acervo[livrosNoAcervo] = livro;
+    //System.out.println("Livro " + livro.getTitulo() + " adicionado no acervo.");
+    //livrosNoAcervo++;
+    //}else {
+    //System.out.println("Acervo lotado!");
+    //}
+
+
+
+public static void main(String[] args) {
+    Biblioteca biblioteca = new Biblioteca();
+    Livro l = new Livro("Java como Programar", "Deitel", 2014);
+    biblioteca.cadastrarLivro(l);
+    biblioteca.cadastrarLivro(l);
+    biblioteca.cadastrarLivro(l);
+    biblioteca.cadastrarLivro(l);
+    biblioteca.cadastrarLivro(l);
+    biblioteca.cadastrarLivro(l);
+}
 }
